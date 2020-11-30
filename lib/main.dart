afc053bc7a663252f5a47a6f96b9597ae32c929a
Folafunmi <folafunmimustapha@gmail.com>
2020-11-30 11:07:06 +0100

    Added dummy initState and dispose methods

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,18 @@ class _HomePageState extends State<HomePage> {
   TextEditingController _nameController = TextEditingController();
 
   @override
+  void initState() {
+    // to initialize something before creating this widget
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // calls this method when this widget is disposed
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[200],
@@ -93,6 +105,7 @@ class _HomePageState extends State<HomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           myText = _nameController.text;
+          // setState needs to be called when
           setState(() {});
         },
         child: Icon(Icons.send),
